ed079ca1742d7798f219f63d66d9ab91e1c8cc96
Henrique Estanislau <65318224+HenriqueEstanislau@users.noreply.github.com>
2022-04-15 17:00:00 -0300

    Update detalhes.js

--- Paladare/Cardapio/detalhes.js
+++ Paladare/Cardapio/detalhes.js
@@ -10,7 +10,6 @@ ajax.addEventListener("readystatechange", function(){
 
         var resposta = ajax.response;        
 
-
         //Pego o ID que foi enviado pelo "produtos.js"
         const urlParams = new URLSearchParams(location.search);
         console.log(urlParams.get('produto'));
@@ -54,57 +53,56 @@ ajax.addEventListener("readystatechange", function(){
 
 
             // If para verificar se a quantidade esta correta
-            if ((qtd >= 1) && (qtd <= 15) && !(parseInt(qtd) != parseFloat(qtd))){
+            if(!isNaN(qtd) && (qtd != "")){
+              if ((qtd >= 1) && (qtd <= 15) && !(parseInt(qtd) != parseFloat(qtd))){
 
-
-              /* 
-              Todo valor do sessionStorage é null no inicio (antes de adicionarmos algum valor nele),
-              Por isso checamos se é null, ou seja, se será o primeiro item a ser adicionado.
-              */
-              
-              if (sessionStorage.getItem('items') === null) {
+                /* 
+                Todo valor do sessionStorage é null no inicio (antes de adicionarmos algum valor nele),
+                Por isso checamos se é null, ou seja, se será o primeiro item a ser adicionado.
+                */
                 
-                // Adicionando um array com um objeto no sessionStorage
-                sessionStorage.setItem('items', JSON.stringify([dataObj]));
+                if (sessionStorage.getItem('items') === null) {
+                  
+                  // Adicionando um array com um objeto no sessionStorage
+                  sessionStorage.setItem('items', JSON.stringify([dataObj]));
 
-              } else {
+                } else {
 
-                // Copiando o array existente no sessionStorage e adicionando o novo objeto ao final.
-                sessionStorage.setItem(
-                  'items',
-                  // O JSON.parse transforma a string em JSON novamente, o inverso do JSON.strigify
-                  JSON.stringify([
-                    ...JSON.parse(sessionStorage.getItem('items')),
-                    dataObj
-                  ])
-                );
+                  // Copiando o array existente no sessionStorage e adicionando o novo objeto ao final.
+                  sessionStorage.setItem(
+                    'items',
+                    // O JSON.parse transforma a string em JSON novamente, o inverso do JSON.strigify
+                    JSON.stringify([
+                      ...JSON.parse(sessionStorage.getItem('items')),
+                      dataObj
+                    ])
+                  );
 
+                }
+
+                // ----------- Efeito splash -----------
+                const splash = document.querySelector('.splash');
+                console.log("entrou")
+                
+                setTimeout(()=>{
+                    splash.classList.add('effect')
+                }, 200)
+                console.log("entrou")
+                setTimeout(()=>{
+                    splash.classList.add('display-none')
+                }, 2000)
+                setTimeout(()=>{
+                    splash.classList.remove('display-none')
+                }, 0)
+                
+              }else{
+                const erro = document.getElementById("error");
+                erro.innerHTML = `* Você não pode adicionar ${qtd} itens`
               }
-
-              // ----------- Efeito splash -----------
-              const splash = document.querySelector('.splash');
-              console.log("entrou")
-              
-              setTimeout(()=>{
-                  splash.classList.add('effect')
-              }, 200)
-              console.log("entrou")
-              setTimeout(()=>{
-                  splash.classList.add('display-none')
-              }, 2000)
-              setTimeout(()=>{
-                  splash.classList.remove('display-none')
-              }, 0)
-              
             }else{
               const erro = document.getElementById("error");
-              erro.innerHTML = `* Você não pode adicionar ${qtd} itens`
-
+              erro.innerHTML = `* Adicione uma quantidade`
             }
-            
         }     
-
-
-
     }
 });
