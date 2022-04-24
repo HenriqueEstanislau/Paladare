9f49a900f08538985102a0cd7ef7dd3a8d39d6ad
Henrique Estanislau <65318224+HenriqueEstanislau@users.noreply.github.com>
2022-04-24 18:08:38 -0300

    Update script.js

--- Paladare/index/script.js
+++ Paladare/index/script.js
@@ -70,3 +70,21 @@ $(document).ready(function(){
         }
     });
 });
+
+// Mostra a quantidade de itens do carrinho
+function atualizaQtdeCart(){
+    let exibeQtdeCart = document.getElementById("cont-cart");
+    let item = JSON.parse(sessionStorage.getItem('items'));
+    let qtde = []
+    if(item != null){
+        item.forEach((item) => {
+            qtde.push(parseInt(item.qtd));  
+        });
+        let total = qtde.reduce((total, qtde) => total + qtde, 0);
+        exibeQtdeCart.innerHTML = `${total}`
+    }else{
+        exibeQtdeCart.innerHTML = `0`
+    }
+}
+
+atualizaQtdeCart();
